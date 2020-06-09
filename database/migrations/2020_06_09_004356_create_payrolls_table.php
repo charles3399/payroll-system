09e8815870451965b2ace350477fc7e590f36029
Charles <bernaldezsay@gmail.com>
2020-06-09 08:50:52 +0800

    added four tables to database: (employees, positions, payrolls, payments)

---
+++ database/migrations/2020_06_09_004356_create_payrolls_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->float('days_work');
+            $table->float('overtime_hrs');
+            $table->integer('late');
+            $table->integer('absences');
+            $table->float('bonuses')->nullable();
+            $table->integer('employees_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
