5460cdb0dbacdada98149d635ffec6114e12cda1
Charles <bernaldezsay@gmail.com>
2020-06-17 17:23:09 +0800

    Modified users table

--- database/migrations/2020_06_09_234552_add_role_to_users_table.php
+++ database/migrations/2020_06_09_234552_add_role_to_users_table.php
@@ -14,7 +14,7 @@ class AddRoleToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role');
+            $table->string('role')->default('admin');
         });
     }
 
