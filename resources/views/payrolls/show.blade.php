f2d14e71c0bb1948182b900a4e6d8c428a98a35e
Charles <c.bernaldez@thedigitalcube.com>
2021-08-09 19:01:16 +0800

    Minor changes for HTML formatting

--- resources/views/payrolls/show.blade.php
+++ resources/views/payrolls/show.blade.php
@@ -6,41 +6,77 @@
     <div class="container">
         <div class="card shadow">
             <div class="card-header d-flex justify-content-between">
-                <h4>Payroll Information for: <strong>{{ $payrolls->employees->pluck('lname')->first() }}, {{ $payrolls->employees->pluck('fname')->first() }}</strong> ({{date_format($payrolls->created_at, 'Y/m/d h:i a')}})</h4>
-                <a class="btn btn-primary" href="{{ route('payrolls.index') }}" role="button"><span><i class="fas fa-long-arrow-alt-left"></i> Back to Payroll List</span></a>
+                <h4>
+                    Payroll Information for: 
+                    <strong>
+                        {{ $payrolls->employees->pluck('lname')->first() }}, {{ $payrolls->employees->pluck('fname')->first() }}
+                    </strong> 
+                    ({{date_format($payrolls->created_at, 'Y/m/d h:i a')}})
+                </h4>
+                <a class="btn btn-primary" href="{{ route('payrolls.index') }}" role="button">
+                    <span>
+                        <i class="fas fa-long-arrow-alt-left"></i> Back to Payroll List
+                    </span>
+                </a>
             </div>
             <div class="card-body">
-                <label><h4>ID: </h4></label>
+                <label>
+                    <h4>ID: </h4>
+                </label>
                 <span style="font-size: 25px">{{ $payrolls->id }}</span><br>
 
-                <label><h4>Employee name: </h4></label>
+                <label>
+                    <h4>Employee name: </h4>
+                </label>
                 <span style="font-size: 25px">
                     {{ $payrolls->employees->pluck('lname')->first() }}, {{ $payrolls->employees->pluck('fname')->first() }}
                 </span><br>
 
-                <label><h4>Position: </h4></label>
+                <label>
+                    <h4>Position: </h4>
+                </label>
                 <span style="font-size: 25px">
                     {{ $position_name->pluck('name')->first() }}
                 </span><br>
 
-                <label><h4>Basic Pay: </h4></label>
+                <label>
+                    <h4>Basic Pay: </h4>
+                </label>
                 <span style="font-size: 25px">
                     ₱{{ number_format($basic_pay->pluck('basic_pay')->first()) }}
                 </span><br>
 
-                <label><h4>Days Worked: </h4></label>
-                <span style="font-size: 25px">{{ $payrolls->days_work }}</span><br>
+                <label>
+                    <h4>Days Worked: </h4>
+                </label>
+                <span style="font-size: 25px">
+                    {{ $payrolls->days_work }}
+                </span><br>
 
-                <label><h4>Overtime (hours): </h4></label>
-                <span style="font-size: 25px">{{ $payrolls->overtime_hrs }}</span><br>
+                <label>
+                    <h4>Overtime (hours): </h4>
+                </label>
+                <span style="font-size: 25px">
+                    {{ $payrolls->overtime_hrs }}
+                </span><br>
 
-                <label><h4>Lates (minutes): </h4></label>
-                <span style="font-size: 25px">{{ $payrolls->late }}</span><br>
+                <label>
+                    <h4>Lates (minutes): </h4>
+                </label>
+                <span style="font-size: 25px">
+                    {{ $payrolls->late }}
+                </span><br>
 
-                <label><h4>Absences: </h4></label>
-                <span style="font-size: 25px">{{ $payrolls->absences }}</span><br>
+                <label>
+                    <h4>Absences: </h4>
+                </label>
+                <span style="font-size: 25px">
+                    {{ $payrolls->absences }}
+                </span><br>
 
-                <label><h4>Bonuses: </h4></label>
+                <label>
+                    <h4>Bonuses: </h4>
+                </label>
                 <span style="font-size: 25px">
                     @if ($payrolls->bonuses == 0)
                         No bonus..
